f42a01925f39f4920717c07e30ed3da763283ed1
badr <badrbm540@gmail.com>
2026-08-07 19:28:36 +0300

    first commit

---
+++ api/src/main/java/dev/badr_m1/image_processor_api/ImageProcessorApiApplication.java
@@ -0,0 +1,13 @@
+package dev.badr_m1.image_processor_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageProcessorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageProcessorApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/dev/badr_m1/image_processor_api/ImageProcessorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.badr_m1.image_processor_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageProcessorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
